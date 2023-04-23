1be40a01af7327a2e5e4bfeaf8a76b585a2ace09
Josh Medeiros <josh@jamedeus.com>
2023-04-23 14:55:44 -0700

    Add unit tests for new QR classes

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from ContactQr import *
+from WifiQr import *
+from LinkQr import *
 from app import app
 import json, os
 
@@ -34,9 +36,29 @@ class EndpointTests(TestCase):
         self.assertTrue(os.path.exists('AzureDiamond_Wifi_QR.png'))
         os.remove('AzureDiamond_Wifi_QR.png')
 
+    def test_wifi_qr_longer_password(self):
+        # Send request, confirm status
+        payload = {'ssid': 'mynet', 'password': 'hunter2', 'type': 'wifi-qr'}
+        response = self.app.post('/generate', json=payload, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+        # Confirm created, clean up
+        self.assertTrue(os.path.exists('mynet_Wifi_QR.png'))
+        os.remove('mynet_Wifi_QR.png')
+
     def test_link_qr(self):
         # Send request, confirm status
         payload = {'url': 'https://jamedeus.com', 'type': 'link-qr'}
+        response = self.app.post('/generate', json=payload, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+        # Confirm created, clean up
+        self.assertTrue(os.path.exists('jamedeus.com_QR.png'))
+        os.remove('jamedeus.com_QR.png')
+
+    def test_http_link_qr(self):
+        # Send request, confirm status
+        payload = {'url': 'http://jamedeus.com', 'type': 'link-qr'}
         response = self.app.post('/generate', json=payload, content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
@@ -51,13 +73,66 @@ class EndpointTests(TestCase):
 class ContactQrTests(TestCase):
 
     def test_contact_qr(self):
-        # Instantiate model
-        qr = ContactQr("Jogn", "Doe", "212-555-1234", "johnathan.doeth@hotmail.com")
+        # Instantiate, confirm attributes
+        qr = ContactQr("John", "Doe", "(212) 555-1234", "johnathan.doeth@hotmail.com")
         self.assertIsInstance(qr, ContactQr)
+        self.assertEqual(qr.first_name, 'John')
+        self.assertEqual(qr.last_name, 'Doe')
+        self.assertEqual(qr.phone, '(212) 555-1234')
+        self.assertEqual(qr.email, 'johnathan.doeth@hotmail.com')
 
-        # Save with non-default name
+        # Save, confirm filename
+        self.assertEqual(qr.filename, 'John-Doe_contact')
+        qr.save()
+        self.assertTrue(os.path.exists('John-Doe_contact.png'))
+
+        # Save with non-default name, confirm created, clean up
         qr.save('New_Name.png')
+        self.assertTrue(os.path.exists('New_Name.png'))
+        os.remove('New_Name.png')
 
-        # Confirm created with custom name, clean up
+    # Test automatic attribute formatting
+    def test_formatting(self):
+        # Instantiate with wrong capitalization, confirm corrected
+        qr = ContactQr("john", "DOE", "   (212) 555-1234 ", "joHnAtHaN.dOEth@hOtmAiL.cOm")
+        self.assertIsInstance(qr, ContactQr)
+        self.assertEqual(qr.first_name, 'John')
+        self.assertEqual(qr.last_name, 'Doe')
+        self.assertEqual(qr.phone, '(212) 555-1234')
+        self.assertEqual(qr.email, 'johnathan.doeth@hotmail.com')
+
+
+class WifiQrTests(TestCase):
+
+    def test_wifi_qr(self):
+        # Instantiate
+        qr = WifiQr("mywifi", "hunter2")
+        self.assertIsInstance(qr, WifiQr)
+
+        # Save, confirm filename
+        self.assertEqual(qr.filename, 'mywifi_Wifi_QR')
+        qr.save()
+        self.assertTrue(os.path.exists('mywifi_Wifi_QR.png'))
+
+        # Save with non-default name, confirm created, clean up
+        qr.save('New_Name.png')
+        self.assertTrue(os.path.exists('New_Name.png'))
+        os.remove('New_Name.png')
+
+
+class LinkQrTests(TestCase):
+
+    def test_link_qr(self):
+        # Instantiate
+        qr = LinkQr("https://jamedeus.com")
+        self.assertIsInstance(qr, LinkQr)
+        self.assertEqual(qr.filename, 'jamedeus.com_QR')
+
+        # Save, confirm filename
+        qr.save()
+        self.assertTrue(os.path.exists('jamedeus.com_QR.png'))
+
+        # Save with non-default name, confirm created, clean up
+        qr.save('New_Name.png')
         self.assertTrue(os.path.exists('New_Name.png'))
         os.remove('New_Name.png')
