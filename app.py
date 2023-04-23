9f4721461bbf15408a5eeb8efc8219ae5628b63c
Josh Medeiros <josh@jamedeus.com>
2023-04-23 13:10:44 -0700

    Remove download endpoint, download button converts base64 string to png. Backend storage no longer required

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, render_template, send_file, jsonify
+from flask import Flask, request, render_template, send_file
 from ContactQr import *
 from WifiQr import *
 from LinkQr import *
@@ -21,13 +21,10 @@ def generate():
 
     if data['type'] == 'contact-qr':
         qr = ContactQr(data['firstName'], data['lastName'], data['phone'], data['email'])
-        filename = f"{qr.first_name}-{qr.last_name}_contact.png"
     elif data['type'] == 'wifi-qr':
         qr = WifiQr(data['ssid'], data['password'])
-        filename = f"{qr.ssid}_Wifi_QR.png"
     elif data['type'] == 'link-qr':
         qr = LinkQr(data['url'])
-        filename = f"{qr.url[8:]}_QR.png"
 
     qr.save()
 
@@ -35,12 +32,8 @@ def generate():
     qr.qr_complete.save(buffered, format="PNG")
     img_bytes = base64.b64encode(buffered.getvalue()).decode("utf-8")
 
-    return jsonify({'filename': filename, 'qr': img_bytes})
-
-@app.get("/download/<name>")
-def download(name):
-    return send_file(f'{name}_contact.png', as_attachment=True)
+    return img_bytes
 
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', debug=True)
+    app.run(host= '0.0.0.0', port=8123, debug=True)
