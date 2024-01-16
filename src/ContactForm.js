af940e223bff57257255a84e7adbe181d1a73de0
Josh Medeiros <josh@jamedeus.com>
2024-01-15 21:53:40 -0800

    React port: Format phone and email fields as user types
    Phone: Format to (xxx) xxx-xxxx
    
    Email: Prevent spaces, allow other characters

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -5,6 +5,35 @@ import Button from 'react-bootstrap/Button';
 
 
 function ContactForm({ onSubmit }) {
+    // Format phone number as user types
+    function formatPhone(event) {
+        // Remove all non-numeric characters, 10 digits max
+        const input = event.target.value.replace(/\D/g,'').substring(0,10);
+
+        // Parse phone number sections
+        const areaCode = input.substring(0,3);
+        const middle = input.substring(3,6);
+        const last = input.substring(6,10);
+
+        // Format based on current length
+        if (input.length > 6) {
+            event.target.value = `(${areaCode}) ${middle}-${last}`;
+        } else if (input.length > 3) {
+            event.target.value = `(${areaCode}) ${middle}`;
+        } else if (input.length > 0) {
+            event.target.value = `(${areaCode}`;
+        } else if (input.length == 0) {
+            event.target.value = ``;
+        }
+    }
+
+    // Allow all characters except spaces in email field
+    function formatEmail(event) {
+        if (event.code === 'Space') {
+            event.preventDefault();
+        }
+    }
+
     return (
         <Form onSubmit={onSubmit}>
             <FloatingLabel label="First Name" className="mb-3">
@@ -16,11 +45,11 @@ function ContactForm({ onSubmit }) {
             </FloatingLabel>
 
             <FloatingLabel label="Email" className="mb-3">
-                <Form.Control type="email" name="email" placeholder="Email" />
+                <Form.Control type="email" name="email" placeholder="Email" onKeyDown={formatEmail} />
             </FloatingLabel>
 
             <FloatingLabel label="Phone" className="mb-3">
-                <Form.Control type="tel" name="phone" placeholder="Phone" />
+                <Form.Control type="tel" name="phone" placeholder="Phone" onChange={formatPhone} />
             </FloatingLabel>
 
             <div className="d-flex">
